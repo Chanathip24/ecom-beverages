407d0be8c74115bc18d7f420aaf3d24acdd1d368
Chanathip <chanathip.cha@outlook.com>
2024-08-29 02:17:42 +0700

    fix page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
 
 //pages
 import Index from './pages/Index'
@@ -13,12 +13,15 @@ function App() {
 
   return (
     <>
-    
+    <Router>
       <Routes>
-        <Route path='/' element={<Index/>}/>
-        <Route path='/login' element={<Login/>} />
-        {/* 404 page */}
-      </Routes>
+          <Route path='/' element={<Index/>}/>
+          <Route path='/login' element={<Login/>} />
+          <Route path='*' element={<Index/>}/>
+          {/* 404 page */}
+        </Routes>
+    </Router>
+
     </>
   )
 }
